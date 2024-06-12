5ea524d0cd998bbdfc0c71e4157838d4c8cb9d20
SeanC2024 <seancwork2024@gmail.com>
2024-06-12 10:59:02 -0700

    .

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let buttonBtn = document.querySelector("button");
 let anchorBtn = document.querySelector(".needyAnchorTag");
 let paragraphBtn = document.querySelector(".needyParagraphTag");
-let myImage = document.querySelector(img);
+let myImage = document.querySelector("img");
 
 // This hides the img tag
 myImage.style.display = 'none';
@@ -10,7 +10,7 @@ myImage.style.display = 'none';
 // the src (i.e., image to be displayed) will be updated.
 buttonBtn.addEventListener("click", function() {
     myImage.style.display = 'block';
-    myImage = "https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg";
+    myImage.src = "https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg";
 });
 
 // When the mouse is over the anchor tag, the img tag will be displayed and 
@@ -22,7 +22,7 @@ anchorBtn.addEventListener("mouseover", function() {
 
 // When the mouse is over the paragraph tag and then leaves, the img tag will be displayed and 
 // the src (i.e., image to be displayed) will be updated.
-paragraphBtn.addEventListener("mouseout") {
+PagraphBtn.addEventListener("mouseout", function() {
     myImage.style.display = 'block';
     myImage.src = "https://cdn.pixabay.com/photo/2014/05/07/06/44/cat-339400_1280.jpg";
 });
